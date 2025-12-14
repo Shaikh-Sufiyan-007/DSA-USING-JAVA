4efb36d98cb8c3b6dae6f9eccbebd2f90b871664
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2025-12-14 19:34:24 +0530

    How search a word in Trie

--- Trie/TrieImplementation.java
+++ Trie/TrieImplementation.java
@@ -27,10 +27,26 @@ public class TrieImplementation {
 
         curr.eow = true;
     }
+
+    public static boolean search(String word) {
+        Node curr = root;
+        for(int level=0; level<word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+
+        return curr.eow == true;
+    }
     public static void main(String[] args) {
         String[] words = {"the", "a", "there", "their", "any", "thee"};
         for(int i=0; i<words.length; i++) {
             insert(words[i]);
         }
+
+        System.out.println(search("the"));
+        System.out.println(search("an"));
     }
 }
