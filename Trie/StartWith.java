9b5ad8aaf05b1b28d2af3a5de03456cf9fff3877
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2025-12-16 19:52:31 +0530

    Starts with problem

---
+++ Trie/StartWith.java
@@ -0,0 +1,54 @@
+package Trie;
+import java.util.*;
+
+public class StartWith {
+    static class Node {
+        Node[] children = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for(int i=0; i<26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for(int level=0; level<word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static boolean search(String word) {
+        Node curr = root;
+        for(int level=0; level<word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        String[] words = {"apple", "mango", "man", "woman"};
+        for(int i=0; i<words.length; i++) {
+            insert(words[i]);
+        }
+
+
+        System.out.println(search("moon"));
+        System.out.println(search("app"));
+    }
+}
